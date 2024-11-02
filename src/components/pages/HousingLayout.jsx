fd86d3499fd14f167621a635c1a3d451be52433c
HiroFr <ichiro.vf@gmail.com>
2024-11-02 22:26:00 +0100

    update : Mobile scss / Housing pages structure reorganize

--- src/components/pages/HousingLayout.jsx
+++ src/components/pages/HousingLayout.jsx
@@ -25,28 +25,29 @@ function HousingLayout() {
     <>
       <NavBar />
       <Carousel />
-      <div className="headerLayout">
-        <div className="titleLocation">
-          <h1>{cardData.title}</h1>
-          <h2>{cardData.location}</h2>
+      <div className="detailsHeader">
+        <div className="detailsHeader__main">
+          <div className="detailsHeader__titleLocation">
+            <h1>{cardData.title}</h1>
+            <h2>{cardData.location}</h2>
+          </div>
+          <div className="detailsHeader__tags">
+            {cardData.tags.map((tagsTitle, index) => (
+              <Tag key={index} tagsTitle={tagsTitle}/>
+            ))}
+          </div>
         </div>
-        <Profile hostname={cardData.host.name} hostpicture={cardData.host.picture}/>
-      </div>
 
-      <div className="tagsRating">
-        <div className="tagsLayout">
-          {cardData.tags.map((tagsTitle, index) => (
-            <Tag key={index} tagsTitle={tagsTitle}/>
-          ))}
+        <div className="detailsHeader__profileRating">
+          <Profile hostname={cardData.host.name} hostpicture={cardData.host.picture}/>
+          <div className="detailsHeader__rating">
+            {stars.map((_, index) => (
+              <StarIcons key={index} colorIcons={cardData.rating > index ? colors.red : colors.grey} />
+            ))}
+          </div>        
         </div>
-        <div className="rating">
-          {stars.map((_, index) => (
-            <StarIcons key={index} colorIcons={cardData.rating > index ? colors.red : colors.grey} />
-          ))}
-        </div>        
       </div>
-
-
+      
       <div className="collapseContent">
         <Collapse title="Description" content={cardData.description}/>
         <Collapse title="Équipements" content={
